60c0ce54ad038f41f4609fdad42a01a825bb3a67
derconno <der_conno@gmx.de>
2023-06-17 18:44:59 +0200

    Fixing stuff to make it work

--- includes/EmailFilterPasswordPrimaryAuthenticationProvider.php
+++ includes/EmailFilterPasswordPrimaryAuthenticationProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-use MediaWiki\Auth\PreAuthenticationProvider;
+use MediaWiki\Auth\AbstractPreAuthenticationProvider;
 use MediaWiki\Auth\AuthenticationRequest;
 use MediaWiki\Auth\UserDataAuthenticationRequest;
 use MediaWiki\Auth\AuthenticationResponse;
@@ -8,23 +8,26 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Status;
 
 class EmailFilterPasswordPrimaryAuthenticationProvider
-	extends PreAuthenticationProvider {
+	extends AbstractPreAuthenticationProvider {
 
 	public function testForAccountCreation( $user, $creator, array $reqs ) {
 		$config = MediaWiki\MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'EmailRegistrationFilter' );
 		$patterns = $config->get( 'PatternList' );
 		$is_whitelist = $config->get( 'IsWhiteList' );
 		$error_msg = $config->get( 'ErrorMessage' );
+		$ret = StatusValue::newGood();
 
 		$userdata_req = AuthenticationRequest::getRequestByClass( $reqs, UserDataAuthenticationRequest::class );
 		if ( $userdata_req->email == null ) {
 			/* Nothing to do on no email. Might want to set $wgEmailConfirmToEdit=true;
 			 * or something like that */
-			return parent::testForAccountCreation($user, $creator, $reqs);
+			if ($is_whitelist) {
+				$ret->fatal($error_msg);	
+			}
+			return $ret;
 		}
 
 		$email = $userdata_req->email;
-		$ret = StatusValue::newGood();
 		$matched = false;
 		foreach ($patterns as $pattern) {
 				if (preg_match($pattern, $email)) {
